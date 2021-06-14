dcca71be25ecdedecaa8c2ad62ca9605b406bd23
Cristian <cristian.enache@invgroup.co.uk>
2021-06-14 13:30:09 +0300

    Initial Commit

---
+++ app/Http/Controllers/DevController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DevController extends Controller
+{
+
+    public function dev(){
+
+        $contents = file_get_contents(storage_path() . '/app/private/covid-19-data/public/data/owid-covid-data.json');
+
+        $contents_arr = json_decode($contents, true);
+
+        $x = 2;
+
+        //return response()->json($contents);
+
+    }
+
+}
